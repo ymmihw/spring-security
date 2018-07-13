267597ace5fe8936d0f4d135bea80764d4a8218a
wanghaoming <whimmy@126.com>
2018-07-13 16:08:32 +0800

    remove code

--- authentication-series/channel-security-https/src/main/java/com/ymmihw/spring/security/MvcConfig.java
+++ authentication-series/channel-security-https/src/main/java/com/ymmihw/spring/security/MvcConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -12,13 +11,6 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addViewControllers(final ViewControllerRegistry registry) {
-    registry.addViewController("/anonymous.html");
-    registry.addViewController("/login.html").setViewName("//login");
-    registry.addViewController("/homepage.html");
-  }
 
   @Bean
   public ViewResolver viewResolver() {
