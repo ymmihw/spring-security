0ff1553f302b0c601d9696c58e3eafea434c85d5
wanghaoming <whimmy@126.com>
2018-08-15 18:07:11 +0800

    add spring-security-oauth2-angularjs

---
+++ spring-security-oauth2/spring-security-oauth2-angularjs/spring-security-oauth2-ui-implicit/src/main/java/org/spring/security/oauth2/ui/implicit/App.java
@@ -0,0 +1,13 @@
+package org.spring.security.oauth2.ui.implicit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
