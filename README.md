ba38442d035d59b63bfef14cf037e0d1d6acfbdf
wanghaoming <whimmy@126.com>
2018-08-16 13:54:14 +0800

    add spring-security-oauth2-jwt-store

--- README.md
+++ README.md
@@ -41,6 +41,7 @@ spring-security-oauth2-sso | [Simple Single Sign On with Spring Security OAuth2]
 spring-security-oauth2-principal-authorities-extractor | [Extracting Principal and Authorities using Spring Security OAuth](https://www.baeldung.com/spring-security-oauth-principal-authorities-extractor)
 spring-security-oauth2-angularjs | [Spring REST API + OAuth2 + AngularJS](http://www.baeldung.com/rest-api-spring-oauth2-angularjs)
 spring-security-oauth2-angularjs | [Angular 4 Upgrade for Spring Security OAuth](http://www.baeldung.com/angular-4-upgrade-for-spring-security-oauth/)
+spring-security-oauth2-jwt-store | [Using JWT with Spring Security OAuth](http://www.baeldung.com/spring-security-oauth-jwt)
 spring-security-oauth2-authorization-code-flow | [Front-End App with Spring Security OAuth – Authorization Code Flow](https://www.baeldung.com/spring-security-oauth-authorization-code-flow)
 ---
 
---
+++ spring-security-oauth2/spring-security-oauth2-jwt-store/resource-server/src/main/java/com/ymmihw/resource/server/config/OAuth2ResourceServerConfigRemoteTokenService.java
@@ -0,0 +1,46 @@
+package com.ymmihw.resource.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class OAuth2ResourceServerConfigRemoteTokenService extends ResourceServerConfigurerAdapter {
+
+  @Override
+  public void configure(final HttpSecurity http) throws Exception {
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+        .authorizeRequests().anyRequest().permitAll();
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JwtTokenStore(accessTokenConverter());
+  }
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+    converter.setSigningKey("123");
+    return converter;
+  }
+
+  @Bean
+  @Primary
+  public DefaultTokenServices tokenServices() {
+    DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+    defaultTokenServices.setTokenStore(tokenStore());
+    return defaultTokenServices;
+  }
+}
