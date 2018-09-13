02df7b975baf0314b9dd1b76800aac8ac8291056
wanghaoming <whimmy@126.com>
2018-09-13 14:58:10 +0800

    add PasswordEncoder

--- spring-security-web/spring-security-web-csrf/src/test/java/com/ymmihw/springsecuritycsrf/config/SecurityWithCsrfConfig.java
+++ spring-security-web/spring-security-web-csrf/src/test/java/com/ymmihw/springsecuritycsrf/config/SecurityWithCsrfConfig.java
@@ -7,51 +7,44 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityWithCsrfConfig extends WebSecurityConfigurerAdapter {
 
-    public SecurityWithCsrfConfig() {
-        super();
-    }
+  @Override
+  protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    auth.inMemoryAuthentication().withUser("user1").password(encoder.encode("user1Pass"))
+        .authorities("ROLE_USER");
+    auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("adminPass"))
+        .authorities("ROLE_ADMIN");
+  }
 
-    // java config
+  @Override
+  public void configure(final WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/resources/**");
+  }
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("user1")
-            .password("user1Pass")
-            .authorities("ROLE_USER")
-            .and()
-            .withUser("admin")
-            .password("adminPass")
-            .authorities("ROLE_ADMIN");
-    }
-
-    @Override
-    public void configure(final WebSecurity web) throws Exception {
-        web.ignoring()
-           .antMatchers("/resources/**");
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.authorizeRequests()
+  @Override
+  protected void configure(final HttpSecurity http) throws Exception {
+    // @formatter:off
+        http.authorizeRequests().antMatchers("/login").permitAll()
             .antMatchers("/auth/admin/*")
             .hasAnyRole("ROLE_ADMIN")
             .anyRequest()
             .authenticated()
             .and()
+            .formLogin().and()
             .httpBasic()
             .and()
             .headers()
             .cacheControl()
             .disable();
         // @formatter:on
-    }
+  }
 
 }
