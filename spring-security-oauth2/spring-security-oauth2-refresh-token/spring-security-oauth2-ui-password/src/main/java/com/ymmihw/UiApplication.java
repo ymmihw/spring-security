a4caad335afc5f0b9a5e6c451e308a7f256b970a
whimmy <whimmy@126.com>
2018-11-05 21:54:05 +0800

    may work

--- spring-security-oauth2/spring-security-oauth2-refresh-token/spring-security-oauth2-ui-password/src/main/java/com/ymmihw/UiApplication.java
+++ spring-security-oauth2/spring-security-oauth2-refresh-token/spring-security-oauth2-ui-password/src/main/java/com/ymmihw/UiApplication.java
@@ -2,11 +2,12 @@ package com.ymmihw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class UiApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
