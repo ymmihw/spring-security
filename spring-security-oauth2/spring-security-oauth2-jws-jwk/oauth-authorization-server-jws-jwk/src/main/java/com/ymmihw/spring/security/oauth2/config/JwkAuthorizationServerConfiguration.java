ebd0cb09fd98248bd23c0304243af89f10018fd9
wanghaoming <wanghaoming@ctfo.com>
2019-09-16 12:46:04 +0800

    format code

--- spring-security-oauth2/spring-security-oauth2-jws-jwk/oauth-authorization-server-jws-jwk/src/main/java/com/ymmihw/spring/security/oauth2/config/JwkAuthorizationServerConfiguration.java
+++ spring-security-oauth2/spring-security-oauth2-jws-jwk/oauth-authorization-server-jws-jwk/src/main/java/com/ymmihw/spring/security/oauth2/config/JwkAuthorizationServerConfiguration.java
@@ -4,7 +4,6 @@ import java.security.KeyPair;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Collections;
 import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -13,7 +12,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.KeyUse;
@@ -23,34 +21,33 @@ import com.nimbusds.jose.jwk.RSAKey;
 @EnableAuthorizationServer
 public class JwkAuthorizationServerConfiguration {
 
-    private static final String KEY_STORE_FILE = "bael-jwt.jks";
-    private static final String KEY_STORE_PASSWORD = "bael-pass";
-    private static final String KEY_ALIAS = "bael-oauth-jwt";
-    private static final String JWK_KID = "bael-key-id";
+  private static final String KEY_STORE_FILE = "bael-jwt.jks";
+  private static final String KEY_STORE_PASSWORD = "bael-pass";
+  private static final String KEY_ALIAS = "bael-oauth-jwt";
+  private static final String JWK_KID = "bael-key-id";
 
-    @Bean
-    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
-        return new JwtTokenStore(jwtAccessTokenConverter);
-    }
+  @Bean
+  public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+    return new JwtTokenStore(jwtAccessTokenConverter);
+  }
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        Map<String, String> customHeaders = Collections.singletonMap("kid", JWK_KID);
-        return new JwtCustomHeadersAccessTokenConverter(customHeaders, keyPair());
-    }
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    Map<String, String> customHeaders = Collections.singletonMap("kid", JWK_KID);
+    return new JwtCustomHeadersAccessTokenConverter(customHeaders, keyPair());
+  }
 
-    @Bean
-    public KeyPair keyPair() {
-        ClassPathResource ksFile = new ClassPathResource(KEY_STORE_FILE);
-        KeyStoreKeyFactory ksFactory = new KeyStoreKeyFactory(ksFile, KEY_STORE_PASSWORD.toCharArray());
-        return ksFactory.getKeyPair(KEY_ALIAS);
-    }
+  @Bean
+  public KeyPair keyPair() {
+    ClassPathResource ksFile = new ClassPathResource(KEY_STORE_FILE);
+    KeyStoreKeyFactory ksFactory = new KeyStoreKeyFactory(ksFile, KEY_STORE_PASSWORD.toCharArray());
+    return ksFactory.getKeyPair(KEY_ALIAS);
+  }
 
-    @Bean
-    public JWKSet jwkSet() {
-        RSAKey.Builder builder = new RSAKey.Builder((RSAPublicKey) keyPair().getPublic()).keyUse(KeyUse.SIGNATURE)
-            .algorithm(JWSAlgorithm.RS256)
-            .keyID(JWK_KID);
-        return new JWKSet(builder.build());
-    }
+  @Bean
+  public JWKSet jwkSet() {
+    RSAKey.Builder builder = new RSAKey.Builder((RSAPublicKey) keyPair().getPublic())
+        .keyUse(KeyUse.SIGNATURE).algorithm(JWSAlgorithm.RS256).keyID(JWK_KID);
+    return new JWKSet(builder.build());
+  }
 }
--- spring-security-oauth2/spring-security-oauth2-jws-jwk/oauth-authorization-server-jws-jwk/src/main/java/com/ymmihw/spring/security/oauth2/config/JwtCustomHeadersAccessTokenConverter.java
+++ spring-security-oauth2/spring-security-oauth2-jws-jwk/oauth-authorization-server-jws-jwk/src/main/java/com/ymmihw/spring/security/oauth2/config/JwtCustomHeadersAccessTokenConverter.java
@@ -4,7 +4,6 @@ import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.jwt.crypto.sign.RsaSigner;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -15,28 +14,28 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 
 public class JwtCustomHeadersAccessTokenConverter extends JwtAccessTokenConverter {
 
-    private Map<String, String> customHeaders = new HashMap<>();
-    private JsonParser objectMapper = JsonParserFactory.create();
-    final RsaSigner signer;
+  private Map<String, String> customHeaders = new HashMap<>();
+  private JsonParser objectMapper = JsonParserFactory.create();
+  final RsaSigner signer;
 
-    public JwtCustomHeadersAccessTokenConverter(Map<String, String> customHeaders, KeyPair keyPair) {
-        super();
-        super.setKeyPair(keyPair);
-        this.signer = new RsaSigner((RSAPrivateKey) keyPair.getPrivate());
-        this.customHeaders = customHeaders;
-    }
+  public JwtCustomHeadersAccessTokenConverter(Map<String, String> customHeaders, KeyPair keyPair) {
+    super();
+    super.setKeyPair(keyPair);
+    this.signer = new RsaSigner((RSAPrivateKey) keyPair.getPrivate());
+    this.customHeaders = customHeaders;
+  }
 
-    @Override
-    protected String encode(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-        String content;
-        try {
-            content = this.objectMapper.formatMap(getAccessTokenConverter().convertAccessToken(accessToken, authentication));
-        } catch (Exception ex) {
-            throw new IllegalStateException("Cannot convert access token to JSON", ex);
-        }
-        String token = JwtHelper.encode(content, this.signer, this.customHeaders)
-            .getEncoded();
-        return token;
+  @Override
+  protected String encode(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+    String content;
+    try {
+      content = this.objectMapper
+          .formatMap(getAccessTokenConverter().convertAccessToken(accessToken, authentication));
+    } catch (Exception ex) {
+      throw new IllegalStateException("Cannot convert access token to JSON", ex);
     }
+    String token = JwtHelper.encode(content, this.signer, this.customHeaders).getEncoded();
+    return token;
+  }
 
 }
