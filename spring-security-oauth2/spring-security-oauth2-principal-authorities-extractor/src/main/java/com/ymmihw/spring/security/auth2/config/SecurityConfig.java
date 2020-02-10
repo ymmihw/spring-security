6415d17b5dd4fc3e102229ba094dba45b8c2bb14
wanghaoaming <whimmy@126.com>
2020-02-10 13:08:59 +0800

    update to security 5.2

--- spring-security-oauth2/spring-security-oauth2-principal-authorities-extractor/src/main/java/com/ymmihw/spring/security/auth2/config/SecurityConfig.java
+++ spring-security-oauth2/spring-security-oauth2-principal-authorities-extractor/src/main/java/com/ymmihw/spring/security/auth2/config/SecurityConfig.java
@@ -1,20 +1,18 @@
 package com.ymmihw.spring.security.auth2.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
   @Override
   protected void configure(HttpSecurity http) throws Exception { // @formatter:off
     http.antMatcher("/**")
           .authorizeRequests()
             .antMatchers("/login**").permitAll()
             .anyRequest().authenticated()
+        .and().oauth2Login()
         .and().formLogin().disable();
   }// @formatter:on
 }
