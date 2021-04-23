c9b7d74bc92504a8051a1610fff3e03e20f9888d
10004160 <10004160@win10-tjgry08.ctfo.local>
2021-04-23 17:57:37 +0800

    fixed

---
+++ spring-security-web/spring-security-web-csrf/src/main/java/com/ymmihw/springsecuritycsrf/controller/FooController.java
@@ -0,0 +1,52 @@
+package com.ymmihw.springsecuritycsrf.controller;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.util.UriComponentsBuilder;
+import com.ymmihw.springsecuritycsrf.dto.Foo;
+
+@Controller
+@RequestMapping(value = "/auth/foos")
+public class FooController {
+
+  public FooController() {
+    super();
+  }
+
+  // API
+
+  // read - single
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  @ResponseBody
+  public Foo findById(@PathVariable("id") final Long id, final UriComponentsBuilder uriBuilder,
+      final HttpServletResponse response) {
+    return new Foo(randomAlphabetic(6));
+  }
+
+  // read - multiple
+
+  @RequestMapping(method = RequestMethod.GET)
+  @ResponseBody
+  public List<Foo> findAll() {
+    return Arrays.asList(new Foo(randomAlphabetic(6)));
+  }
+
+  // write - just for test
+  @RequestMapping(method = RequestMethod.POST)
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public Foo create(@RequestBody final Foo foo) {
+    return foo;
+  }
+}
